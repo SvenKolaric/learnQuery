4579d5ede87d39f59a5fb3173cab9d0ee06166a6
Sven Kolarić <sven.kolaric@intellexi.hr>
2020-06-03 23:20:02 +0200

    Linted version of cssPropsTs

--- 02.CSS_manipulation/src/cssPropTs.ts
+++ 02.CSS_manipulation/src/cssPropTs.ts
@@ -1,14 +1,17 @@
-const cssProp = (element: HTMLElement, cssProperty: string | { [index: string]: string }, value?: string) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const cssProp = (element: HTMLElement,
+  cssProperty: string | { [index: string]: string },
+  value?: string) => {
   const styles = element.style;
   if (!value) {
-    if (typeof cssProperty === "string") {
+    if (typeof cssProperty === 'string') {
       return styles.getPropertyValue(cssProperty);
-    } else {
-      Object.keys(cssProperty).forEach(key => {
-        styles.setProperty(key, cssProperty[key])
-      });
     }
+    Object.keys(cssProperty).forEach((key) => {
+      styles.setProperty(key, cssProperty[key]);
+    });
   } else {
     styles.setProperty(cssProperty as string, value);
   }
-}
+  return null; // eslint -> Expected to return a value at the end of arrow function
+};
