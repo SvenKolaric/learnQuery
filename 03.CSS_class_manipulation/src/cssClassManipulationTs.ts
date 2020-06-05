9dac2d4c688bf1b20536676ffd9ef11b8d08f1b1
Sven Kolarić <sven.kolaric@intellexi.hr>
2020-06-05 20:43:10 +0200

    Refactor task solution

--- 03.CSS_class_manipulation/src/cssClassManipulationTs.ts
+++ 03.CSS_class_manipulation/src/cssClassManipulationTs.ts
@@ -1,29 +1,27 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-class cssClass {
-  static add = (
+const cssClass = {
+  add(
     htmlElement: HTMLElement,
     className: string,
-  ) => {
+  ) {
     htmlElement.classList.add(className);
-  };
-
-  static remove = (
+  },
+  remove(
     htmlElement: HTMLElement,
     className: string,
-  ) => {
+  ) {
     htmlElement.classList.remove(className);
-  };
-
-  static toggle = (
+  },
+  toggle(
     htmlElement: HTMLElement,
     className: string,
-  ) => {
+  ) {
     htmlElement.classList.toggle(className);
-  };
-
-  static has = (
+  },
+  has(
     htmlElement: HTMLElement,
     className: string,
-  ) => htmlElement.classList.contains(className);
-}
+  ) {
+    return htmlElement.classList.contains(className);
+  },
+};
