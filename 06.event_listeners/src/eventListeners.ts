5f9d373fa84af937ee46564408b7817b3153dff6
Sven Kolarić <sven.kolaric@intellexi.hr>
2020-06-18 16:22:44 +0200

    Refactor the removeEvent

--- 06.event_listeners/src/eventListeners.ts
+++ 06.event_listeners/src/eventListeners.ts
@@ -1,29 +1,32 @@
-interface Data {
-  events: Map<string, EventListener[]>
+interface EventsMap {
+  events: Map<string, CallbackData>
 }
 
-const elementMap: Map<HTMLElement, Data> = new Map<HTMLElement, Data>();
+interface CallbackData {
+  callbacks: EventListener[],
+  eventHandler: EventListener,
+}
+
+const elementMap: Map<HTMLElement, EventsMap> = new Map<HTMLElement, EventsMap>();
 
 function eventHandler(event: Event) {
   // eslint-disable-next-line prefer-destructuring
   const target = <HTMLElement>event.target;
   if (target && elementMap.has(target) && elementMap.get(target)?.events.has(event.type)) {
     const callbackArray = elementMap.get(target)!.events.get(event.type);
-    callbackArray!.forEach((callback) => {
+    callbackArray!.callbacks.forEach((callback) => {
       callback(event);
     });
   }
 }
 
-function removeEventsForCallbackArray(
+function removeEvent(
   element: HTMLElement,
   event: string,
-  callbackArray: EventListener[],
+  eventHandlerEvn: EventListener,
 ) {
-  if (element && event && callbackArray) {
-    callbackArray.forEach((callback) => {
-      element.removeEventListener(event, callback);
-    });
+  if (element && event && eventHandlerEvn) {
+    element.removeEventListener(event, eventHandlerEvn);
   }
 }
 
@@ -32,42 +35,42 @@ const eventListener = {
   on(element: HTMLElement, event: string, callback: EventListener) {
     if (!elementMap.has(element)) {
       elementMap.set(element, {
-        events: new Map([[event, [callback]]]),
+        events: new Map([[event, { callbacks: [callback], eventHandler }]]),
       });
     } else {
       const mapEvent = elementMap.get(element)!.events;
       if (!mapEvent.has(event)) {
-        mapEvent.set(event, [callback]);
+        mapEvent.set(event, { callbacks: [callback], eventHandler });
       } else {
-        mapEvent.get(event)!.push(callback);
+        mapEvent.get(event)!.callbacks.push(callback);
       }
     }
     element.addEventListener(event, eventHandler);
   },
 
-  off(element: HTMLElement, event?: string, callback?: EventListener) {
+  off(element: HTMLElement, event?: string, callback?: EventListener) { // jako off
     if (elementMap.has(element)) {
-      if (event && elementMap.get(element)!.events.has(event)) {
-        const callbackArray = elementMap.get(element)!.events.get(event);
-        if (callbackArray) {
-          if (callback) {
-            const index = callbackArray.indexOf(callback, 0);
+      if (event && elementMap.get(element)!.events.has(event)) { //  pojednostavi event je vazna provjera
+        const callbackData = elementMap.get(element)!.events.get(event);
+        if (callbackData) {
+          if (callback) { // vazna provjera
+            const index = callbackData.callbacks.indexOf(callback, 0);
             if (index > -1) {
-              removeEventsForCallbackArray(element, event, [callback]);
-              callbackArray.splice(index, 1);
-              if (callbackArray.length === 0) {
+              callbackData.callbacks.splice(index, 1);
+              if (callbackData.callbacks.length === 0) {
                 elementMap.get(element)!.events.delete(event);
+                removeEvent(element, event, callbackData.eventHandler);
               }
             }
           } else {
-            removeEventsForCallbackArray(element, event, callbackArray);
+            removeEvent(element, event, callbackData.eventHandler);
             elementMap.get(element)!.events.delete(event);
           }
         }
       } else {
         elementMap.forEach((elementValue, elementKey) => {
           elementValue.events.forEach((eventValue, eventKey) => {
-            removeEventsForCallbackArray(elementKey, eventKey, eventValue);
+            removeEvent(elementKey, eventKey, eventValue.eventHandler);
           });
         });
         elementMap.clear();
